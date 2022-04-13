6c5de893dcd587952f5af7474d1e3ce41a26f449
Lucenix <andrelucenaferreira@gmail.com>
2022-04-13 11:05:06 +0100

    mais comentários

--- sdstore.c
+++ sdstore.c
@@ -11,9 +11,11 @@ int main(int argc, char const *argv[])
         write(1, "./sdstore proc-file priority input-filename output-filename transformation-id-1 transformation-id-2 ...\n", 105);
         return 0;
     } else if (strcmp(argv[1], "status") == 0) {
-        //do status
+        //do status, pedir ao servidor o status das tasks em execução e dos limites
     } else if (strcmp(argv[1], "proc-file") == 0) {
-        //do procfile
+        //do procfile, pode ou não ter prioridade, importa? não basta mandar ao servidor e ele depois manda de volta para cá
+        //nada de forks e execs, o servidor já está aberto
+        //esperar pelas informações do servidor? para imprimir pending, processing e concluded
     }
     return 0;
 }
