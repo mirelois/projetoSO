40e0c353ed24e008c8b3899cfee692462c18f2ed
mirelois <79458822+mirelois@users.noreply.github.com>
2022-04-22 10:09:45 +0100

    i am freeeeee, free falling, also readHT

--- hashtable.c
+++ hashtable.c
@@ -34,12 +34,13 @@ int plusOneHT(HT *h, char key[], int *value) {
     return p;
 }
 
-/*
-int freeHT(HT *h, int k) {
-    
-    return 
+int freeHT(HT *h) {
+    int p;
+    for(p = 0; p < h->size; p++){
+        free(h->tbl+p);
+    }
+    return 0;
 }
-*/
 
 int isprime(int p){
     int i , r = 0;
@@ -90,30 +91,19 @@ int writeHT (HT *h, char key[], int value) {
 
         *h = *new_h;
 
-        free(new_h);//maybe not enough
+        free(new_h);
         
         return 0;
     }
 
 }
 
-
-
-/*
-int writeHT(HT *h, char key[], int value){
-    HT new_h;
-    if(((h->used)/(h->size)) <= 0.80){
-
-    }
-}
-*/
-
 int readHT(HT *h, char key[], int* value){
     int p , r = -1 , c = h->size, flg;
     for(p = hash(key,h->size); strcmp(key,(h->tbl)[p].key) && (c > 0) && (flg = strcmp(EMPTY,(h->tbl)[p].key)); c--){
         p = (p+1)%(h->size);
     }
-    if(c != 0 && !flg){
+    if(c != 0 && flg){
         r = p;
         *value = (h->tbl)[p].value;
     }
