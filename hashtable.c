0cab0d3ba228ef5c075e9e2cf4526ec4ae50acc0
mirelois <79458822+mirelois@users.noreply.github.com>
2022-04-22 10:55:23 +0100

    comentarios

--- hashtable.c
+++ hashtable.c
@@ -4,6 +4,15 @@
 
 #include "hashtable.h"
 
+/**
+ * @brief funcao de hash
+ * 
+ * soma o valor de ascii de todos os caracteres de uma string
+ * 
+ * @param key string a qual a funçao sera aplicada
+ * @param size tamanho do dicionario
+ * @return 
+ */
 int hash(char key[], int size) {
     int ret = 0;
     for(int i =0; key[i] != '\0'; i++){
@@ -12,17 +21,39 @@ int hash(char key[], int size) {
     return ret%size;
 }
 
-
-void initHT(HT *h, int size) {
+/**
+ * @brief inicializa um dicionario
+ * 
+ * 
+ * @param h pointer de um dicionario a ser inicializado
+ * @param size tamanho do dicionario
+ * @return 0 se o dicionario for inicializado corretamente e -1 caso contrario
+ */
+int initHT(HT *h, int size) {
     h->tbl = calloc(size, sizeof(struct pair));
+
+    if(h->tbl == NULL){
+        return -1;
+    }
+
     h->size = size;
     h->used = 0;
     for (int i=0; i<size; i++) {
         strcpy(h->tbl[i].key, EMPTY);
         h->tbl[i].value = -1;
     }
+    return 0;
 }
 
+/**
+ * @brief incrementa o valor associado a uma chave por um 
+ * 
+ * 
+ * @param h pointer de um dicionario onde atualizar o valor
+ * @param key chave cujo valor associado sera incrementado
+ * @param value pointer de um valor onde guardar o valor incrementado
+ * @return posicao onde o valor foi incrementado ou -1 de falhar
+ */
 int plusOneHT(HT *h, char key[], int *value) {
 
     int x;
@@ -34,25 +65,47 @@ int plusOneHT(HT *h, char key[], int *value) {
     return p;
 }
 
-int freeHT(HT *h) {
+/**
+ * @brief liberta a memoria de um dicionario 
+ * 
+ * 
+ * @param h pointer de um dicionario a libertar
+ * @return 0 se p nao e primo 1 caso contrario
+ */
+void freeHT(HT *h) {
     int p;
     for(p = 0; p < h->size; p++){
         free(h->tbl+p);
     }
-    return 0;
 }
 
+/**
+ * @brief verifica se um numero é primo 
+ * 
+ * 
+ * @param p valor a testar
+ * @return 0 se p nao e primo 1 caso contrario
+ */
 int isprime(int p){
     int i , r = 0;
     for(i = 2 ; i < p/2 ; i++){
-        if(p%i == 0){r = 1;}
+        if(p%i == 0) {
+            r = 1;
+        }
     }
     return r;
 }
 
 
-//assume que ha espaço
-//nao é dinamico
+/**
+ * @brief funcao auxiliar do writeHT 
+ * 
+ * 
+ * @param h pointer para o dicionario onde escrever
+ * @param key chave a colocar no dicionario
+ * @param value valor a associar com a chave no dicionario
+ * @return posicao onde escreveu
+ */
 int writeHTaux (HT *h, char key[], int value) {
 
     int p= hash(key, h->size);
@@ -66,9 +119,21 @@ int writeHTaux (HT *h, char key[], int value) {
     return p;
 }
 
-int writeHT (HT *h, char key[], int value) {
 
-    writeHTaux(h, key, value);
+/**
+ * @brief escreve um par chave valor no dicionario 
+ * 
+ * quando a carga passa de MAX_CHARGE o tamanho do 
+ * dicionario é aumentado para o menor numero primo maior do que o dobro do tamanho atual
+ * 
+ * assume que chave nao ocorre no dicionario
+ * 
+ * @param h pointer para o dicionario onde escrever
+ * @param key chave a colocar no dicionario
+ * @param value valor a associar com a chave no dicionario
+ * @return posiçao onde colocou o par ou -1 se falhar
+ */
+int writeHT (HT *h, char key[], int value) {
 
     float charge = ((float)(h->used))/(h->size);
 
@@ -76,11 +141,17 @@ int writeHT (HT *h, char key[], int value) {
 
         HT *new_h = malloc(sizeof(HT));
 
+        if(new_h == NULL){
+            return -1;
+        }
+
         int new_size;
 
         for(new_size = (h->size)*2; !isprime(new_size); new_size++);
 
-        initHT(new_h, new_size+1);
+        if(initHT(new_h, new_size+1) == -1){
+            return -1;
+        }
 
         for(int i = 0; i < h->size; i++) {
             
@@ -88,16 +159,25 @@ int writeHT (HT *h, char key[], int value) {
             
         }
 
-
         *h = *new_h;
 
-        free(new_h);
+        freeHT(new_h);
         
-        return 0;
     }
 
+    return writeHTaux(h, key, value);
 }
 
+/**
+ * @brief lê o valor associado a uma chave
+ * 
+ * assume que chave so aparece uma ves no dicionario
+ * 
+ * @param h pointer para o dicionario de onde ler
+ * @param key chave
+ * @param value pointer onde é colocado o valor associado a chave
+ * @return posicao na tabela de onde foi lido o valor ou -1 caso a chave nao exista no dicionario
+ */
 int readHT(HT *h, char key[], int* value){
     int p , r = -1 , c = h->size, flg;
     for(p = hash(key,h->size); strcmp(key,(h->tbl)[p].key) && (c > 0) && (flg = strcmp(EMPTY,(h->tbl)[p].key)); c--){
@@ -110,6 +190,15 @@ int readHT(HT *h, char key[], int* value){
     return r;
 }
 
+/**
+ * @brief subtitui a chave por DELETED e coloca o valor guardado a -1
+ * 
+ * assume que chave so aparece uma ves no dicionario
+ * 
+ * @param h pointer para o dicionario em que sera eliminado o valor
+ * @param key chave a eliminar
+ * @return posicao na tabela de onde foi eliminada a chave ou -1 caso a chave nao exista no dicionario
+ */
 int deleteHT (HT *h, char key[]) {
     int x;
     int p = readHT(h, key, &x);
--- hashtable.h
+++ hashtable.h
@@ -18,41 +18,16 @@ typedef struct {
 } HT;
 
 
-// sum of hascii values hash function
 int hash(char key[], int size);
 
+void freeHT(HT *h);
 
-// ENSURES array is allocated and initialized with key EMPTY everywhere
-void initHT (HT *h, int size);
+int initHT (HT *h, int size);
 
-//adds one to value with key = key
 int plusOneHT(HT *h, char key[], int* value);
 
-// REQUIRES key does not occur in table h (no defensive code)
-// (alternatives would be to check defensively, OR to use an update semantics whenever key occurs already)
-// ENSURES pair (key, value) is inserted in table h
-//         uses linear probing and inserts in next EMPTY or DELETED position
-//         returns array index where pair is inserted
-//         if load becomes higher than 80%, table is reallocated 
-//         ("double" size / nearest prime number)
 int writeHT (HT *h, char key[], int value);
 
-
-
-//  since writeHT does not implement an update semantics, one way to update is to use read to return index and do a "hard update"
-
-
-// REQUIRES key occurs at most once in table h
-// ENSURES returns array position where key is found or -1 otherwise
-//         (this allows the function to be used as auxiliary for update / removal)
-//         writes value associated with key in *value
-//         uses linear probing treating DELETED positions as occupied
 int readHT (HT *h, char key[], int* value);
 
-
-
-// ASSUMES key occurs at most once in table h
-// ENSURES returns array position where key was found or -1 otherwise
-//         marks position as DELETED
-//         uses linear probing treating DELETED positions as occupied
 int deleteHT (HT *h, char key[]);
