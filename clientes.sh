47632e8eac5c53ab56f82df842717d567464d133
Carlos Machado <74628557+hiddenduck@users.noreply.github.com>
2022-05-25 22:13:38 +0100

    scripting like a truck

--- clientes.sh
+++ clientes.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-for((i=0; i<20; i++))
+for((i=0; i<10; i++))
 do
-./sdstore proc-file in out1 nop &
-./sdstore proc-file in out1 nop &
-./sdstore proc-file in out1 nop &
-./sdstore proc-file in out1 nop &
-./sdstore proc-file in out1 nop &
+./sdstore proc-file large.test out1 encrypt &
+./sdstore proc-file in out2 bcompress &
+./sdstore proc-file in out3 nop encrypt &
+./sdstore proc-file in out4 encrypt nop &
+./sdstore proc-file in out5 encrypt decrypt &
 ./sdstore status &
-./sdstore proc-file in out1 nop &
-./sdstore proc-file in out1 nop &
+./sdstore proc-file in out6 bcompress bdecompress &
+./sdstore proc-file in out7 nop nop nop &
 done
